9598280cddd23652938ac12f4ae12d79f8ac02d4
Ken <kensindytomjerry@gmail.com>
2024-08-10 12:47:33 -0500

    contact

--- js/bos.js
+++ js/bos.js
@@ -5,6 +5,7 @@ const bp1Container = document.getElementById('bp1Container');
 const bp2Container = document.getElementById('bp2Container');
 
 bp1.addEventListener('click', function() {
+    if (window.innerWidth < 860) return;
     bp2.classList.remove('businessHeaderSelected');
     bp1.classList.add('businessHeaderSelected');
     bp1Container.classList.remove('hidden');
@@ -14,6 +15,7 @@ bp1.addEventListener('click', function() {
 bp2.addEventListener('click', function() {
     bp1.classList.remove('businessHeaderSelected');
     bp2.classList.add('businessHeaderSelected');
-    bp2Container.classList.remove('hidden');
+    bp2Container.style.display = "block";
     bp1Container.classList.add('hidden');
+
 });
