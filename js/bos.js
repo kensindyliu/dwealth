8fc31fc35aa8d7fe142ec4939cbd1d48491c87a7
Ken <kensindytomjerry@gmail.com>
2024-08-13 14:38:55 -0500

    add ib.html and more

--- js/bos.js
+++ js/bos.js
@@ -3,19 +3,20 @@ const bp1 = document.getElementById('bp1');
 const bp2 = document.getElementById('bp2');
 const bp1Container = document.getElementById('bp1Container');
 const bp2Container = document.getElementById('bp2Container');
-const bp3 = document.getElementById('bp3');
+
 bp1.addEventListener('click', function() {
     if (window.innerWidth < 860) return;
     bp2.classList.remove('businessHeaderSelected');
     bp1.classList.add('businessHeaderSelected');
-    bp1Container.classList.remove('hidden');
-    bp2Container.classList.add('hidden');
+    bp1Container.style.display = 'block';
+    bp2Container.style.display = 'none';
+    
 });
 
 bp2.addEventListener('click', function() {
+    if (window.innerWidth < 860) return;
     bp1.classList.remove('businessHeaderSelected');
     bp2.classList.add('businessHeaderSelected');
     bp2Container.style.display = "block";
-    bp1Container.classList.add('hidden');
-    bp3.classList.add('hidden');
+    bp1Container.style.display = 'none';
 });
